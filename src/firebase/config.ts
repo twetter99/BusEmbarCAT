559516652d5ce7a18d92de0726ca7dc160a5f070
Juan Blanco <blancosampedro@gmail.com>
2025-10-31 11:14:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3788379378-70ad0",
+  "appId": "1:203511370251:web:6290edc9fd25f757616d0e",
+  "apiKey": "AIzaSyDFQmFkhLpRfdzV3pdjlrJ426NfnNfi4i8",
+  "authDomain": "studio-3788379378-70ad0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "203511370251"
+};
